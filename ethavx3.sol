768f4c0c4acac4b04656b791aa17ea0002180a55
Abhayjeet23 <137137987+Abhayjeet23@users.noreply.github.com>
2023-08-13 14:43:00 +0530

    Update ethavx3.sol

--- ethavx3.sol
+++ ethavx3.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MyToken {
+contract AbhayToken {
     string public tokenName;
     string public tokenSymbol;
     uint256 public totalTokenSupply;
